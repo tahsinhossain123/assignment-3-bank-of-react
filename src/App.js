e0e9621f84b3f9388ec8c720a2d7977ee2dbace7
amira-abdelmohsen <amira15abdelmohsen@gmail.com>
2024-04-08 19:36:26 -0400

    deployment packages

--- src/App.js
+++ src/App.js
@@ -84,7 +84,7 @@ class App extends Component {
 
     // Important: Include the "basename" in Router, which is needed for deploying the React app to GitHub Pages
     return (
-      <Router basename="/bank-of-react-starter-code">
+      <Router basename="/bank-of-react">
         <div>
           <Route exact path="/" render={HomeComponent}/>
           <Route exact path="/userProfile" render={UserProfileComponent}/>
